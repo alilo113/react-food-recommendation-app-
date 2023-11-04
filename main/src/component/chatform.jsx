b8fc9332a9814cada56b1703e5115312b2fd53ae
unknown <aliopdandan@gmail.com>
2023-11-04 20:04:19 +0100

    commit

--- main/src/component/chatform.jsx
+++ main/src/component/chatform.jsx
@@ -7,25 +7,34 @@ export function Chatform() {
 
   function addRes(e) {
     e.preventDefault();
-
-    // Assuming 'userInput' is the message entered by the user
-    const userMessage = userInput.trim(); // Trim any extra spaces
-
-    if (userMessage.toLowerCase() === "yes") {
-      const newMessage = {
-        id: crypto.randomUUID(),
-        message: userMessage,
-      };
-
-      setResponse(prevResponse => [...prevResponse, newMessage]);
+  
+    let updatedResponse = [...response]; // Create a copy of the existing response array
+  
+    if (userInput.toUpperCase() === "YES" || userInput.toUpperCase() === "ANOTHER") {
+      updatedResponse.push({ id: crypto.randomUUID(), message: userInput, isBot: false }); // User message
+      updatedResponse.push({ id: crypto.randomUUID(), message: getBotResponse(), isBot: true }); // Bot response
+    } 
+    else if(userInput.toUpperCase() === "no"){
+      updatedResponse.push({ id: crypto.randomUUID(), message: userInput, isBot: false }); // User message
+      updatedResponse.push({id: crypto.randomUUID(), message: "Ok let me know if you change your mind", isBot: true})
     }
+  
+    setResponse(updatedResponse); // Update the response state once with the modified array
+    setUserInput(""); // Clear the input field
+  }
+  
 
-    setUserInput("");
+  function getBotResponse() {
+    const foodArray = [
+      "pizza", "burger", "salad", "sushi", "pasta", "tacos", "ice cream", "steak",
+      "ramen", "tempura", "sashimi", "udon", "tonkatsu", "gyudon", "okonomiyaki", "yakitori"
+    ];    
+    return `${foodArray[Math.floor(Math.random() * foodArray.length)]}`;
   }
 
   return (
     <div>
-      <Chatwindow response={response} />
+      <Chatwindow response={response} userInput={userInput} />
       <form onSubmit={addRes}>
         <input
           type="text"
--- main/src/component/chatwindow.jsx
+++ main/src/component/chatwindow.jsx
@@ -1,21 +1,15 @@
 export function Chatwindow({ response }) {
-  const foodArray = ["pizza", "burger", "salad", "sushi", "pasta", "tacos", "ice cream", "steak"];
-  const randomFood = foodArray[Math.floor(Math.random() * foodArray.length)];
-
   return (
     <div id="chat-window" className="chat-window">
       <p>
-        <b>Bot: </b>Are you hungry? I can fix that. Type YES if you want something to eat or NO if you don't.
+        <b>Bot: </b>Are you hungry? I can fix that. Type YES if you want something to eat or NO if you don't and type ANOTHER to give you another thing to eat.
       </p>
-      {response && Array.isArray(response) && response.map((responses) => (
-        <>
-        <p key={responses.id}>
-          <b>Me: </b>{responses.message}
-        </p>
-        <p key={responses.id}>
-          <b>Bot: </b>{randomFood}
-        </p>
-      </>
+      {response && Array.isArray(response) && response.map((item) => (
+        <div key={item.id}>
+          <p>
+            {item.isBot ? 'Bot' : 'Me'}: {item.message}
+          </p>
+        </div>
       ))}
     </div>
   );
